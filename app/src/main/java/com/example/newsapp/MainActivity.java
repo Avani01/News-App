59df154aca695ae2121b54347c35636c60e5096c
Avani01 <avaniverma4401@gmail.com>
2022-01-26 11:56:55 +0400

    Part 1

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -1,14 +1,82 @@
 package com.example.newsapp;
-
+/*
+* first go to toi then go to rss feed in top right (wifi symbol). it is xml file of current live tech news
+* code beautify is opened. go to xml viewer. load the url of the xml file.
+* tags needed from it are title, description, publishing date
+* */
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Xml;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private RecyclerView recyclerView;
+    private TextView textTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        textTitle = findViewById(R.id.textTitle);
+    }
+
+    private class GetNews extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            InputStream inputStream = getInputStream();
+            if (null != inputStream) {
+                try {
+                    initXMLPullParser(inputStream);
+                } catch (XmlPullParserException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        private InputStream getInputStream() {
+            try {
+                URL url = new URL("https://timesofindia.indiatimes.com/rssfeeds/66949542.cms");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setDoInput(true);
+                connection.getInputStream();
+            }
+
+            /*catch (MalformedURLException e) {
+                e.printStackTrace();
+            } */
+
+            // malformed url exception extends io exception so no need to specifically give malformed url exception
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        private void initXMLPullParser(InputStream inputStream) throws XmlPullParserException {
+            Log.d(TAG, "initXMLPullParser: Initializing XML Pull Parser");
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(inputStream, null);
+        }
     }
 }
